3a0af76b5679cab1cbea3b3844791f2e004770f1
wrongholt <wrongholt@gmail.com>
2017-10-26 08:46:45 -0500

    switched to bootstrap

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function getParameterByName(name, url) {
+	    if (!url) url = window.location.href;
+	    name = name.replace(/[\[\]]/g, "\\$&");
+	    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+	        results = regex.exec(url);
+	    if (!results) return null;
+	    if (!results[2]) return '';
+	    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	}
+	// Give the parameter a variable name
+	var dynamicContent = getParameterByName('dc');
+console.log(dynamicContent)
+$(document).ready(function() {
+	
+	// Check if the URL parameter is apples
+	if (dynamicContent == 'apples') {
+		$('#apples').show();
+	} 
+	// Check if the URL parameter is oranges
+	else if (dynamicContent == 'oranges') {
+		$('#oranges').show();
+	} 
+	// Check if the URL parameter is bananas
+	else if (dynamicContent == 'bananas') {
+		$('#bananas').show();
+	}
+});
