5bbbf2af672072cc8e2936f1244411e7ec504c16
wrongholt <wrongholt@student.cvtc.edu>
2019-02-14 12:01:07 -0600

    redoing heading

--- js/main.js
+++ js/main.js
@@ -13,3 +13,12 @@ window.fbAsyncInit = function() {
   js.src = "//connect.facebook.net/en_US/sdk.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+
+var vara = new Vara("#container", "https://rawcdn.githack.com/akzhy/Vara/ed6ab92fdf196596266ae76867c415fa659eb348/fonts/Satisfy/SatisfySL.json", [{
+  text: "William Rongholt",
+  fontSize: 48,
+  y:10
+}], {
+  strokeWidth: 2,
+  textAlign:"center"
+});
