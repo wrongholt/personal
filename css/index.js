7067cafb81f52121c7545039ca3d3bb59f95f68f
Will <wrongholt@gmail.com>
2019-02-25 22:29:49 -0600

    fixed errors again

--- css/index.js
+++ css/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 console.log('starting function');
 const AWS = require('aws-sdk');
 const docClient =  new AWS.DynamoDB.DocumentClient({region: 'central'});
@@ -17,24 +16,4 @@ exports.handle = function(e, ctx, callback){
             callback(null, data);
         }
     })
-=======
-console.log('starting function');
-const AWS = require('aws-sdk');
-const docClient =  new AWS.DynamoDB.DocumentClient({region: 'central'});
-exports.handle = function(e, ctx, callback){
-    var params = {
-        Item:{
-            date: Date.now(),
-            message: "I love your website!"
-        },
-        TableName: 'guestbook'
-    };
-    docClient.put(params, function(err, data){
-        if(err){
-            callback(err, null);
-        }else{
-            callback(null, data);
-        }
-    })
->>>>>>> 088c3b65cd37de50c9240f38b50db2c5ee92c780
 }
