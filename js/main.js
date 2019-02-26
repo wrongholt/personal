898fcea4fb467077b35daac39bc0d08f1cddada0
Will <wrongholt@gmail.com>
2019-02-25 21:53:03 -0600

    init

---
+++ js/main.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '255764971238848',
+    xfbml      : true,
+    version    : 'v2.6'
+  });
+};
+ 
+(function(d, s, id){
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+var vara = new Vara("#container", "https://rawcdn.githack.com/akzhy/Vara/ed6ab92fdf196596266ae76867c415fa659eb348/fonts/Satisfy/SatisfySL.json", [{
+  text: "William Rongholt",
+  fontSize: 48,
+  y:10
+}], {
+  strokeWidth: 2,
+  textAlign:"center"
+});
